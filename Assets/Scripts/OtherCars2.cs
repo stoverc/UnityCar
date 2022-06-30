fb81d4960c9f86f8d272da22dabd175239b3299f
Chris Stover <cstover@wolfram.com>
2022-06-30 01:23:35 -0400

    Initial Commit
    Testing an initial upload of a Unity project.

---
+++ Assets/Scripts/OtherCars2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherCars2 : MonoBehaviour
+{
+    // Private Vars
+    private float speed = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * UnityEngine.Random.value * speed);
+    }
+}
